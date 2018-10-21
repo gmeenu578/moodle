011a292d341d2badbbe10b7310eec13a0a71d5f0
gmeenu578 <gmeenu578@gmail.com>
2018-10-21 22:46:14 +0530

    cookies done

---
+++ static/studentdata/js/studentdata.js
@@ -0,0 +1,35 @@
+function fetch()
+{
+	document.getElementById('loader').style.visibility="hidden";
+	document.getElementById('table').style.visibility="visible";
+	document.getElementById('logout').style.visibility="visible";
+}
+
+function clearcookies(cname)
+{
+	var cookies = document.cookie.split(";");
+
+	for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = cname + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT ; path = /;";
+    }
+    window.location = "login.html";
+}
+
+function getCookies(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
